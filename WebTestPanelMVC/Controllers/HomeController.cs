dcee609cfa62798edf9308dc94de99eab8fcc582
anilorhan <anilorhanmail@gmail.com>
2022-07-26 12:48:31 +0300

    12:48

--- WebTestPanelMVC/Controllers/HomeController.cs
+++ WebTestPanelMVC/Controllers/HomeController.cs
@@ -113,11 +113,39 @@ namespace WebTestPanelMVC.Controllers
             return View(_model);
         }
 
+       
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        //[HttpPost("printTest")]
+        //public async IActionResult PrintTest(List<IFormFile> files)
+        //{
+        //    long size = files.Sum(f => f.Length);
+
+        //    var filePaths = new List<string>();
+        //    foreach (var formFile in files)
+        //    {
+        //        if (formFile.Length > 0)
+        //        {
+        //            // full path to file in temp location
+        //            var filePath =
+        //                Path.GetTempFileName(); //we are using Temp file name just for the example. Add your own file path.
+        //            filePaths.Add(filePath);
+        //            using (var stream = new FileStream(filePath, FileMode.Create))
+        //            {
+        //                await formFile.CopyToAsync(stream);
+        //            }
+        //        }
+
+        //        return View(_model);
+        //    }
+
+        //    return Ok(new {count=files.Count,size,filePaths});
+        //}
+      
     }
 }
